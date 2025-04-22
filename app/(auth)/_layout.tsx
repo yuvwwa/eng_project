ad9d52c54d609d9acf2320dc92de2a3a354ba8cb
GorevA <artgor005@gmail.com>
2025-04-22 23:19:15 +0800

    fix: auth_layout

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,15 @@
+import { Stack } from 'expo-router';
+
+export default function AuthLayout() {
+  return (
+    <Stack
+      screenOptions={{
+        animation: 'slide_from_right',
+        gestureEnabled: true, // возвращение назад, под вопросом
+        headerShown: false, // navheader, нахуй не нужен)
+      }}>
+      <Stack.Screen name="login" />
+      <Stack.Screen name="register" />
+    </Stack>
+  );
+}
